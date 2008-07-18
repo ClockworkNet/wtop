8a4b33fb5c51cd44fe99110612dfd75095a04574
aristus <aristus@8ccb5fd8-cb4f-0410-94c8-17fdda874ba6>
2008-07-18 15:47:52 +0000

    new test plan
    git-svn-id: http://wtop.googlecode.com/svn/trunk@60 8ccb5fd8-cb4f-0410-94c8-17fdda874ba6

---
+++ test.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+# basic correctness and performance checks. Essentially we
+# have a list of invocations and check the md5 of their 
+# output and running time against a previously-saved run.
+
+prefix = './logrep -c wtop.cfg'
+suffix = 'access.log | md5 -q'
+tests = (
+    "-o 'url,msec,bot'",
+    "-o 'url,msec,ts'",
+    "-o 'url,msec,class'",
+    "-o 'url,msec,bot,ts,class'",
+    "-f 'url!~/q/,msec>500' -o 'url,msec,bot,ts,class'",
+    "-f 'url!~/q/,msec<500' -o 'url,msec,bot,ts,class,bytes'",
+    "-f 'url!~/q/,msec<500,bot=1' -o 'url,msec,bot,ts,class,bytes'",
+)
+
+
+import time, os, sys, cPickle
+from subprocess import call
+
+def run_tests(tests, prefix, suffix):
+    for t in tests:
+        op.popen()
