245cdb345e114b333d207f9bc92557049807cb3f
Tim Zehta <tim@clockwork.net>
2013-07-19 16:52:38 -0500

    updated setup.py--sorted, added back license

--- setup.py
+++ setup.py
@@ -12,20 +12,22 @@ cfg_file_path = logrep.cfg_home()
 with open('README.rst') as file:
     long_description = file.read()
 
-setup(name="wtop",
-      version=logrep.VERSION,
+setup(
       author="Timid Robot Zehta",
       author_email="tim@clockwork.net",
-      url="https://github.com/ClockworkNet/wtop",
-      description="running statistics for webservers, plus powerful "
-                  "log-grepping tools",
-      long_description=long_description,
-      download_url="https://github.com/ClockworkNet/wtop/releases#egg=wtop",
       classifiers=["Environment :: Console",
                    "License :: OSI Approved :: BSD License",
                    "Topic :: System :: Logging",
                    "Topic :: Utilities"],
       data_files=[(cfg_file_path, ["wtop.cfg"])],
-      scripts=["wtop", "logrep", "wtop.bat", "logrep.bat"],
+      description="running statistics for webservers, plus powerful "
+                  "log-grepping tools",
+      download_url="https://github.com/ClockworkNet/wtop/releases#egg=wtop",
+      license="BSD",
+      long_description=long_description,
+      name="wtop",
       py_modules=["logrep"],
+      scripts=["wtop", "logrep", "wtop.bat", "logrep.bat"],
+      url="https://github.com/ClockworkNet/wtop",
+      version=logrep.VERSION,
 )
