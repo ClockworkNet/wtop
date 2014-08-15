8c40805935c422e7581cd09c2d26ea1ef5440fed
Tim Zehta <tim@clockwork.net>
2014-08-15 09:07:43 -0500

    updated for python 2.7 compatiblity and style
    - flake8 compliant

--- robots/trie.py
+++ robots/trie.py
@@ -1,27 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#author:         rex
-#blog:           http://iregex.org
-#filename        tr.py
-#created:        2010-08-01 20:24
-#source uri:     http://iregex.org/blog/trie-in-python.html
-
-# escape bug fix by fcicq @ 2012.8.19
+# author:         rex
+# blog:           http://iregex.org
+# filename        tr.py
+# created:        2010-08-01 20:24
+# source uri:     http://iregex.org/blog/trie-in-python.html
 
 import re
+
+
 class Trie():
     """Regexp::Trie in python"""
 
     def __init__(self):
-        self.data={}
+        self.data = dict()
 
     def add(self, word):
-        ref=self.data
+        ref = self.data
         for char in word:
-            ref[char]=ref.has_key(char) and ref[char] or {}
-            ref=ref[char]
-        ref['']=1
+            ref[char] = char in ref and ref[char] or dict()
+            ref = ref[char]
+        ref[""] = 1
 
     def dump(self):
         return self.data
@@ -30,48 +30,48 @@ class Trie():
         return re.escape(char)
 
     def _regexp(self, pData):
-        data=pData
-        if data.has_key("") and len(data.keys())==1:
+        data = pData
+        if "" in data and len(data.keys()) == 1:
             return None
 
-        alt=[]
-        cc=[]
-        q=0
+        alt = list()
+        cc = list()
+        q = 0
         for char in sorted(data.keys()):
-            if isinstance(data[char],dict):
+            if isinstance(data[char], dict):
                 try:
-                    recurse=self._regexp(data[char])
+                    recurse = self._regexp(data[char])
                     alt.append(self.quote(char)+recurse)
                 except:
                     cc.append(self.quote(char))
             else:
-                q=1
-        cconly=not len(alt)>0
+                q = 1
+        cconly = not len(alt) > 0
 
-        if len(cc)>0:
-            if len(cc)==1:
+        if len(cc) > 0:
+            if len(cc) == 1:
                 alt.append(cc[0])
             else:
-                alt.append('['+''.join(cc)+']')
+                alt.append("[%s]" % "".join(cc))
 
-        if len(alt)==1:
-            result=alt[0]
+        if len(alt) == 1:
+            result = alt[0]
         else:
-            result="(?:"+"|".join(alt)+")"
+            result = "(?:%s)" % "|".join(alt)
 
         if q:
             if cconly:
-                result+="?"
+                result += "?"
             else:
-                result="(?:%s)?" % result
+                result = "(?:%s)?" % result
         return result
+
     def regexp(self):
-        # return "(?-xism:%s)" % self._regexp(self.dump()) # fcicq: not sure what is it...
         return self._regexp(self.dump())
 
-if __name__ == '__main__':
-  a=Trie()
 
-  for w in ['foobar', 'foobah', 'fooxar', 'foozap', 'fooza']:
-      a.add(w)
-  print a.regexp()
+if __name__ == '__main__':
+    a = Trie()
+    for w in ['foobar', 'foobah', 'fooxar', 'foozap', 'fooza']:
+        a.add(w)
+    print a.regexp()
