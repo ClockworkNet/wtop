355bfac02d086abc57420238664cf559b228744b
Tim Zehta <tim@clockwork.net>
2014-08-15 12:53:27 -0500

    updated to create robots resources
    - renamed generate_robots_regex.py to create_robots_resources.py
    - now creates the following files:
      - uasparser.xml: downloaded xml file
      - robots_excerpt.ini: optimized regex for wtop config file
      - robot_user_agents.txt: robot user agents for testing/validation
    - ignored the above created files

--- robots/create_robots_resources.py
+++ robots/create_robots_resources.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
 """Create an optimized regex for matching robot user agents based on the data
 compiled by http://user-agent-string.info/"""
 # Standard library
 import argparse
+import io
 import re
 import urllib
 from xml.dom.minidom import parse
@@ -13,7 +15,9 @@ import trie
 uasparser_xml_uri = "http://user-agent-string.info/rpc/get_data.php"
 uasparser_xml_query = "key=free&format=xml&download=y"
 download_xml_url = "%s?%s" % (uasparser_xml_uri, uasparser_xml_query)
-downloaded_xml_file = "uasparser.xml"
+file_uasparser_xml = "uasparser.xml"
+file_robots_config = "robots_excerpt.ini"
+file_user_agents = "robot_user_agents.txt"
 others = ["curl", "gsa-crawler", "wget"]
 default_xml_file = ""
 re_trailing_version = re.compile(r"""
@@ -47,12 +51,15 @@ def add_others_to_robots(robots, others_list):
 def add_xml_names_to_robots(robots, xml_file):
     """Add robots from "Data for UASparser" in XML format to robots set."""
     dom = parse(xml_file)
+    user_agents = list()
     for robot in dom.getElementsByTagName("robot"):
         for node in robot.childNodes:
             if node.localName == "family":
                 family = node.firstChild.data.strip()
             if node.localName == "name":
                 name = node.firstChild.data.strip()
+            if node.localName == "useragent":
+                user_agents.append(node.firstChild.data.strip())
         # Use the family title instead of the name, if it is long enough
         if len(family) > 3:
             name = family
@@ -63,7 +70,7 @@ def add_xml_names_to_robots(robots, xml_file):
         if name.endswith(" b"):
             name = name.rstrip(" b")
         robots.add(name.strip())
-    return robots
+    return robots, sorted(user_agents)
 
 
 def create_optmized_robots_regex(robots):
@@ -82,13 +89,21 @@ def main():
     if args.file:
         xml_file = args.file
     else:
-        download_uasparser_xml(download_xml_url, downloaded_xml_file)
-        xml_file = downloaded_xml_file
+        download_uasparser_xml(download_xml_url, file_uasparser_xml)
+        xml_file = file_uasparser_xml
 
     robots = add_others_to_robots(robots, others)
-    robots = add_xml_names_to_robots(robots, xml_file)
+    robots, user_agents = add_xml_names_to_robots(robots, xml_file)
     optibots = create_optmized_robots_regex(robots)
-    print optibots
+
+    # write out robots config excerpt
+    with io.open(file_robots_config, "w", encoding="utf8") as fh:
+        fh.write("robots=%s\n" % optibots)
+
+    # write out robot users agents for testing
+    with io.open(file_user_agents, "w", encoding="utf8") as fh:
+        for agent in user_agents:
+            fh.write("%s\n" % agent)
 
 
 if __name__ == "__main__":
